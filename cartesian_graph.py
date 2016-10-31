7d003c49dd76e5f1033993f8bc36a3a9f2422af4
Shumba Brown <shumbabrownjc@gmail.com>
2016-10-31 18:12:25 -0400

    Structure of function outlined

---
+++ cartesian_graph.py
@@ -0,0 +1,15 @@
+def ConvertToCartesianGraph(points):
+    # Accepts a list of points and return a list of (x, y)
+    # values that represent the points on a cartesian graph
+
+    
+    noOfSpokes = len(points)
+    angleOfSpokes = 360 / noOfSpokes
+    listOfCartesianPoints = []
+    x = 0
+    y = 0
+
+    for point in points:
+        # Calculate (x, y) value
+
+    return listOfCartesianPoints
