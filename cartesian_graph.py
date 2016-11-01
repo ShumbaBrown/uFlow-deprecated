8a5ba2b3cde0590997c1c2ab6ec7e7b92f56e157
Shumba Brown <shumbabrownjc@gmail.com>
2016-11-01 01:29:33 -0400

    commit repo

---
+++ cartesian_graph.py
@@ -0,0 +1,29 @@
+import math
+import graph
+
+
+def ConvertToCartesianGraph(points):
+    # Accepts a list of points and return a list of (x, y)
+    # values that represent the points on a cartesian graph
+
+    noOfSpokes = len(points)
+    angleOfSpokes = 360 / noOfSpokes
+    listOfCartesianPoints = []
+    
+    theta = 0
+
+    for point in points:
+        # Calculate (x, y) Values
+        sin = round(math.sin(math.radians(theta)))
+        cos = round(math.cos(math.radians(theta)))
+        x = (point * cos) - (0 * sin)
+        y = (0 * cos) + (point * sin)
+        cartesianPoint = [x, y]
+        listOfCartesianPoints.append(cartesianPoint)
+        theta += angleOfSpokes
+
+    graph.graphPoints(listOfCartesianPoints)
+    return listOfCartesianPoints
+
+V = [100, 75, 50, 100, 100, 25, 50, 75]
+ConvertToCartesianGraph(V)
